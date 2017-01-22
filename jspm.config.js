17e4a217b787f363426d5d249863835ae340db35
Alexis Vincent <alexisjohnvincent@gmail.com>
2017-01-22 16:45:16 +0200

    Added jspm specific mappings and deps

---
+++ jspm.config.js
@@ -0,0 +1,23 @@
+SystemJS.config({
+  browserConfig: {
+    "paths": {
+      "npm:": "/jspm_packages/npm/"
+    }
+  },
+  nodeConfig: {
+    "paths": {
+      "npm:": "jspm_packages/npm/"
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {
+    "deepmerge": "npm:deepmerge@1.3.1"
+  },
+  packages: {}
+});

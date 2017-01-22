a1c99855ca4c42a47d5defdbfd8ab4738e51e354
Alexis Vincent <alexisjohnvincent@gmail.com>
2017-01-22 17:44:10 +0200

    Fixed up production client

--- dist/client-production.js
+++ dist/client-production.js
@@ -3,37 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-// import 'systemjs-hmr/next'
-// import Rx from 'rxjs/Rx'
-// import {Observable as O} from 'rxjs/Observable'
-
-var tools = {
-  config: {
-    protocol: 'https://',
-    port: 7777,
-    hostname: window.location.hostname
-  }
-};
-
 var passiveInit = exports.passiveInit = function passiveInit() {
-  var c = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-  if (!tools._.initialized) {
-
-    tools.config = Object.assign({}, tools.config, c);
-
-    tools.connect = function () {
-      var configOverride = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-    };
-    // state.
-  }
-  return tools;
+  return {
+    config: {
+      protocol: 'https://',
+      port: 7777,
+      hostname: window.location.hostname
+    },
+    connect: function connect() {}
+  };
 };
 
 var connect = exports.connect = function connect() {
-  var configOverride = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-  var tools = passiveInit(configOverride);
-  tools.connect();
-  return tools;
+  return passiveInit();
 };
